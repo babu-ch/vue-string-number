389300239efb5b0e32e230b552c6b19a4350a470
babu-ch <mashirohira@gmail.com>
2025-01-31 23:56:38 +0900

    add test

---
+++ src/test/stringNumberRef.test.ts
@@ -0,0 +1,6 @@
+import { expect, it } from 'vitest';
+
+it('add', () => {
+  expect(1).toBe(1)
+  expect(6).toBe(6)
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import {dirname, resolve} from 'node:path'
 import {fileURLToPath} from 'node:url'
 import { defineConfig } from 'vite'
@@ -9,6 +10,9 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), dts({tsconfigPath:"./tsconfig.app.json"})],
+  test: {
+    include:["src/test/stringNumberRef.test.ts"]
+  },
   build: {
     lib: {
       entry: resolve(__dirname, 'src/stringNumberRef.ts'),
