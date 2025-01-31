40ca09610a06b44c569ad48b3b0f5ab773898cc6
babu-ch <mashirohira@gmail.com>
2025-01-31 23:48:11 +0900

    vite-plugin-dts

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@ import {dirname, resolve} from 'node:path'
 import {fileURLToPath} from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), dts({tsconfigPath:"./tsconfig.app.json"})],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/stringNumberRef.ts'),
